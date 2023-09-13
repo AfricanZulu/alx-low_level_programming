45b47d41edef6853f79c879886da908086b49044
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-09-13 02:36:09 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
     int r;
 
-    r = isalpha('H');
+    r = -isalpha('H');
     _putchar(r + '0');
-    r = isalpha('o');
+    r = -isalpha('o');
     _putchar(r + '0');
-    r = isalpha(108);
+    r = -isalpha(108);
     _putchar(r + '0');
-    r = isalpha(';');
+    r = -isalpha(';');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
