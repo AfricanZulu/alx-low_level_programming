890c0ac7282013087150c65e07d66b7f4fae2a02
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-09-14 15:03:39 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,7 +16,9 @@ void print_diagonal(int n )
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
-			_putchar('\\');
+		{
+			if (j == i)
+		       	_putchar('\\');
 			else if (j < i)
 			_putchar(' ');
 		}
