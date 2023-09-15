be3fcc1b41c930a1ec56c34c1effdd4658d327bd
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-09-15 14:38:56 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - function checks for a digit (0 through 9)
+ *
  * Return: Always 0.
  */
-
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 48; c < 58; c++)
 	{
 	       	_putchar(c);
         }
-	_putchar('\n');
+	        _putchar('\n');
 }
