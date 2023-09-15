70befd553341227b1b291da057fc514f161e40d1
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-09-15 14:10:49 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,11 +13,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		num >= -num;
+		num = -num;
 	}
 
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
-	_putchar((unsigned int) num % 10 + '0');
+	_putchar((num % 10) + '0');
 }
