edb6168112342d14ee7180efa9a745b0130a68d2
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-09-20 10:22:37 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,22 +5,19 @@
  * @n: integer params
  * Return: 0
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
 
 	n1 = n;
-
 	if (n < 0)
 	{
 		putchar('-');
 		n1 = -n;
 	}
-
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
