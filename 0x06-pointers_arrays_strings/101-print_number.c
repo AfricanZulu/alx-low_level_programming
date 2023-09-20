ded7f2ee41c87da95fbfeb3b73508cf562f000a3
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-09-20 09:43:53 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,13 +14,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n1 = -n;
 	}
 
 	if (n1 / 10 != 0)
 	{
-		print_numnber(n1 / 10);
+		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	putchar((n1 % 10) + '0');
 }
